f72c8225b550d4835a66f960509dbd3069a26135
camarly <camarly@icloud.com>
2022-11-24 21:14:03 -0500

    completed part 3

--- packet_analyser_p3.py
+++ packet_analyser_p3.py
@@ -21,6 +21,29 @@ from packet_analyser_p2 import *
 
 
 
+def flowAverage(pkt_list):
+    """This metric will accept a list of packets and gets the average payload size of all the packets. It will return a list of packets that are above the average of the list."""
+    sumPayload = 0
+    for packet in pkt_list:
+        sumPayload += getPayloadSize(packet)
+    avgPayload = sumPayload / len(pkt_list)
+    return [packet for packet in pkt_list if getPayloadSize(packet) > avgPayload]
+
+
+def suspPort(pkt):
+    """This metric will flag any packets with a source or destination port number that exceeds 500."""
+    return getSrcPort(pkt) > 500 or getDstPort(pkt) > 500
+
+
+def suspProto(pkt):
+    """Suspicious Protocols – There are particular protocols that may be marked as suspicious. These protocols may indicate nefarious activity. In our tool we will reference a list of regular protocol and if the protocol of a given packet is not a prat of that list then the pact will be marked as suspicious."""
+    return getProtocol(pkt) in ProtocolList
+
+    
+
+def ipBlacklist(pkt):
+    """IP Blacklist – Throughout the SSSGO there are known Ip address that are infamous for sending malicious software, spam attacks or even denial of series attacks. These packets should be discarded immediately. Write a function to check if the source IP address of a packet matches any of the IP addresses in the IP backlist."""
+    return getPacketSrc(pkt) in IpBlackList
 
 
 
