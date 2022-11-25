e5fb3577852356452d6ac0dadc49efa4d5b62f72
camarly <camarly@icloud.com>
2022-11-24 20:25:10 -0500

    Packet Analyserr part 2, part3

--- packet_analyser_p2.py
+++ packet_analyser_p2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # All Rights Reserved - No usage allowed
-# File              : packet_analyser_p1.py
+# File              : packet_analyser_p2.py
 # Author            : Camarly Thomas 
 # Author            : Michael Leighton
 
@@ -15,31 +15,8 @@ Member 2: 620146318
 # Last Modified Date: 24.11.2022
 # Last Modified By  : Camarly Thomas 
 
+from packet_analyser_p1.py import * 
 
-def makePacket(srcIP, dstIP, length, prt, sp, dp, sqn, pld):
-    """Takes Packet information and returns a Packet as a tuple, where the first part of the tuple is a tag ‘PK’."""
-    return ("PK", srcIP, dstIP, [length, prt, [sp, dp], sqn, pld])
-    
-def getPacketSrc(pkt):
-    """Takes a Packet as input and returns the source IP address of the Packet."""
-    return pkt[1]
-    
-def getPacketDst(pkt):
-    """Takes a Packet as input and returns the destination IP address of the Packet."""
-    return pkt[2]
-    
-def getPacketDetails(pkt):
-    """Takes a Packet as input and returns the list with the Packet details information."""
-    return pkt[3]
-    
-def isPacket(pkt):
-    """Checks to see if a given Packet, is a valid Packet."""
-    return type(pkt) == tuple and pkt[0] == "PK" and len(pkt) == 4 and type(getPacketDetails(pkt)) == list
-
-
-def isEmptyPkt(pkt):
-    """Checks to see if a given Packet is empty."""
-    return isPacket(pkt) and getPacketDetails(pkt) == []
 
 def getLength(pkt):
     """Takes a Packet as input and returns the Length of the Packet in bytes."""
---
+++ packet_analyser_p3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# All Rights Reserved - No usage allowed
+# File              : packet_analyser_p3.py
+# Author            : Camarly Thomas 
+# Author            : Michael Leighton
+
+"""
+Group Information: 
+Member 1: 620158933 
+Member 2: 620146318
+"""
+
+# Date              : 24.11.2022
+# Last Modified Date: 24.11.2022
+# Last Modified By  : Camarly Thomas 
+
+from packet_analyser_p1.py import * 
+from packet_analyser_p2.py import * 
+
+
+
+
+
+
+
